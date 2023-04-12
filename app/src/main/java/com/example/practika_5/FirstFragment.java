cf1f0a09c305cce18bf042bff1303cffeaa29534
Sokolova Mariia <mashasokolova7@gmail.com>
2023-04-12 11:35:49 +0300

    Передача данных во второй фрагмент, переход во второй фрагмент

--- app/src/main/java/com/example/practika_5/FirstFragment.java
+++ app/src/main/java/com/example/practika_5/FirstFragment.java
@@ -2,57 +2,31 @@ package com.example.practika_5;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FirstFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FirstFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button button_number;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button button_adress;
 
-    public FirstFragment() {
-        // Required empty public constructor
-    }
+    EditText editText_number;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FirstFragment newInstance(String param1, String param2) {
-        FirstFragment fragment = new FirstFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    EditText editText_adress;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +34,32 @@ public class FirstFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        button_number = view.findViewById(R.id.button_number);
+        button_adress = view.findViewById(R.id.button_adress);
+
+        editText_number = view.findViewById(R.id.phoneNumber);
+        editText_adress = view.findViewById(R.id.adress);
+
+        button_number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String peredachaNomera = editText_number.getText().toString();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("nomer", peredachaNomera);
+
+                Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment, bundle);
+            }
+        });
+
+
     }
 }
