8f08afc51f941dfb163f3b66f75d96e0a2b8efea
Sokolova Mariia <mashasokolova7@gmail.com>
2023-04-14 16:52:40 +0300

    1 часть 6 практики, реализация уведомления

--- app/src/main/java/com/example/practika_5/FirstFragment.java
+++ app/src/main/java/com/example/practika_5/FirstFragment.java
@@ -1,9 +1,19 @@
 package com.example.practika_5;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -19,6 +29,10 @@ public class FirstFragment extends Fragment {
     Button button_number;
 
     Button button_adress;
+
+    Button button_notification;
+
+    Button button_banner;
 
     EditText editText_number;
 
@@ -44,6 +58,8 @@ public class FirstFragment extends Fragment {
 
         button_number = view.findViewById(R.id.button_number);
         button_adress = view.findViewById(R.id.button_adress);
+
+        button_notification = view.findViewById(R.id.button_notification);
 
         editText_number = view.findViewById(R.id.phoneNumber);
         editText_adress = view.findViewById(R.id.adress);
@@ -73,6 +89,47 @@ public class FirstFragment extends Fragment {
 
             }
         });
+
+        // *Канал (уведомления)*
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "my channel";
+            String description = "it's the channel for notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("CHANNEL", name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = requireContext().
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent notificationIntent = new Intent(getActivity(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(),
+                0, notificationIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+        button_notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // конструктор уведомеления
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(requireContext(),
+                        "CHANNEL")
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentText("Девушка по вызову в пути!")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                // ПОЛУЧАЕМ МЕНЕДЖЕРА КАПСОМ
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(requireContext());
+
+                //проверяем есть ли разрешение на отправку уведомления
+                if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+
+                // покажись_уведомление
+                notificationManager.notify(1, builder.build());
+            }
+        });
+
 
 
     }
